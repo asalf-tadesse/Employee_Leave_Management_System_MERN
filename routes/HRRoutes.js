3b8a51adbd25f8f79d4132ab864a8650734b3c39
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-09 20:53:34 +0300

    Hr routes

--- routes/HRRoutes.js
+++ routes/HRRoutes.js
@@ -3,3 +3,34 @@ const routers=express.Router();
 module.exports=routers;
 
 // HR routers 
+routers.post('/api/hr/employees/register', (req, res) => {
+    res.send('emp register page');
+});
+
+routers.get('/api/hr/employees/:id', (req, res) => {
+    res.send('view emp page');
+});
+
+routers.put('/api/hr/employees/register/:id', (req, res) => {
+    res.send('edit emp page');
+});
+
+routers.delete('/api/hr/register/:id', (req, res) => {
+    res.send('del emp page');
+});
+
+routers.post('/api/hr/leave/policy', (req, res) => {
+    res.send('create policy page');
+});
+
+routers.get('/api/hr/leave/policy', (req, res) => {
+    res.send('put policy page');
+});
+
+routers.put('/api/hr/leave/policy', (req, res) => {
+    res.send('policy update page');
+});
+
+app.patch('/api/hr/leave/request', (req, res) => {
+    res.send('leave patch page');
+});
